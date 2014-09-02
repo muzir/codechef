3f699bfed22b77eedd2c43a0167fe47593a9f80a
muzir <baycelik_erhun@hotmail.com>
2014-09-02 08:35:13 +0300

    Second question

---
+++ src/main/java/org/muzir/codechef/practice/easy/ATM.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * Pooja would like to withdraw X $US from an ATM. The cash machine will only
+ * accept the transaction if X is a multiple of 5, and Pooja's account balance
+ * has enough cash to perform the withdrawal transaction (including bank
+ * charges). For each successful withdrawal the bank charges 0.50 $US.
+ * 
+ * Calculate Pooja's account balance after an attempted transaction. Input
+ * 
+ * Positive integer 0 < X <= 2000 - the amount of cash which Pooja wishes to
+ * withdraw.
+ * 
+ * Nonnegative number 0<= Y <= 2000 with two digits of precision - Pooja's
+ * initial account balance. Output
+ * 
+ * Output the account balance after the attempted transaction, given as a number
+ * with two digits of precision. If there is not enough money in the account to
+ * complete the transaction, output the current bank balance. Example -
+ * Successful Transaction
+ * 
+ * Input: 30 120.00
+ * 
+ * Output: 89.50
+ * 
+ * Example - Incorrect Withdrawal Amount (not multiple of 5)
+ * 
+ * Input: 42 120.00
+ * 
+ * Output: 120.00
+ * 
+ * Example - Insufficient Funds
+ * 
+ * Input: 300 120.00
+ * 
+ * Output: 120.00
+ * 
+ * 
+ * @author muzir
+ *
+ */
+class Account {
+	private int withdraw;
+	private double balance;
+
+	public int getWithdraw() {
+		return withdraw;
+	}
+
+	public void setWithdraw(int withdraw) {
+		this.withdraw = withdraw;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [withdraw=" + withdraw + ", balance=" + balance + "]";
+	}
+
+	double isWithdrawSuccessful() {
+		if (isWithdrawMultiplyFive()) {
+			this.balance = isSuffiecientFund();
+		}
+		return balance;
+	}
+
+	private boolean isWithdrawMultiplyFive() {
+		if (withdraw % 5 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	private double isSuffiecientFund() {
+		if (!(balance - withdraw < 0.5)) {
+			balance = balance - withdraw - 0.5;
+		}
+		return balance;
+	}
+}
+
+public class ATM {
+
+	public static void main(String[] args) throws IOException {
+		Account account = new Account();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+		String withdraw = tokenizer.nextToken(" ");
+		String balance = tokenizer.nextToken(" ");
+		account.setWithdraw(Integer.parseInt(withdraw));
+		account.setBalance(Double.parseDouble(balance));
+		System.out.println(account);
+		System.out.println(account.isWithdrawSuccessful());
+		reader.close();
+	}
+}
