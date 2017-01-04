eefa6defd4f01c756c876549484c9a867793d8de
muzir <baycelik_erhun@hotmail.com>
2017-01-04 23:22:41 +0200

    -- CHRL2 is not solved yet.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/CHRL2.java
@@ -0,0 +1,71 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author erhun.baycelik
+ *
+ */
+public class CHRL2 {
+
+	private static boolean isCodechefModeOn = false;
+	private static int iterateIndex = 0;
+	private static char[] CHEF = { 'C', 'H', 'E', 'F' };
+
+	public static void main(String[] args) throws IOException {
+		InputStream in = createInputStream();
+		BufferedInputStream reader = new BufferedInputStream(in);
+		DataInputStream dis = new DataInputStream(reader);
+		String input = readLine(dis);
+		System.out.println(solution(input));
+	}
+
+	private static InputStream createInputStream() throws FileNotFoundException {
+		if (isCodechefModeOn) {
+			return System.in;
+		}
+		String path = System.getProperty("user.dir");
+		InputStream in = new FileInputStream(path + "/CHRL2.txt");
+		return in;
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private static String readLine(DataInputStream reader) throws IOException {
+		return reader.readLine();
+	}
+
+	public static int solution(String s) {
+		int counter = 0;
+		char[] input = s.toCharArray();
+		int lenght = input.length;
+		StringBuilder temp = new StringBuilder();
+		for (int i = 0; i < lenght; i++) {
+			char c = input[i];
+			char nextChar = getNextChar();
+			if (c == nextChar) {
+				temp.append(String.valueOf(c));
+				iterateIndex++;
+			}
+			if (temp.toString().equals("CHEF")) {
+				counter++;
+				temp = new StringBuilder();
+				iterateIndex = 0;
+			}
+		}
+		return counter;
+	}
+
+	private static char getNextChar() {
+		if (iterateIndex > 3) {
+			iterateIndex = 0;
+		}
+		return CHEF[iterateIndex];
+	}
+
+}
---
+++ src/test/java/org/muzir/codechef/practice/beginner/CHRL2Test.java
@@ -0,0 +1,25 @@
+package org.muzir.codechef.practice.beginner;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author erhun.baycelik
+ *
+ */
+public class CHRL2Test {
+
+	@Test
+	public void test() {
+		String s = "CHEFCHEFFFF";
+		assertEquals(2, CHRL2.solution(s));
+	}
+
+	@Test
+	public void test1() {
+		String s = "CHHHEEEFFCC";
+		assertEquals(1, CHRL2.solution(s));
+	}
+
+}
