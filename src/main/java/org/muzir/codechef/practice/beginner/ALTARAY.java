34d7e8b94ef79d8ee15a1893f6b6036b512e3c7a
muzir <baycelik_erhun@hotmail.com>
2017-01-21 15:28:20 +0200

    -- ALTARAY is solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/ALTARAY.java
@@ -0,0 +1,90 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ALTARAY {
+	private static boolean isCodechefModeOn = false;
+
+	public static void main(String[] args) throws IOException {
+		InputStream in = createInputStream();
+		BufferedInputStream reader = new BufferedInputStream(in);
+		DataInputStream dis = new DataInputStream(reader);
+		int testCaseCount = Integer.parseInt(readLine(dis));
+		for (int i = 0; i < testCaseCount; i++) {
+			readLine(dis);
+			String line = readLine(dis);
+			int[] a = getArrayOfString(line);
+			System.out.println(solution(a));
+		}
+	}
+
+	static String solution(int[] a) {
+		int lenght = a.length;
+		// If array length is 1 return "1"
+		if (lenght == 1) {
+			return "1";
+		}
+		int[] output = new int[lenght];
+		int base = 0;
+		for (int i = 0; i < lenght - 1; i++) {
+			int p1 = a[i];
+			int p2 = a[i + 1];
+			output[i] = 1;
+			output[i + 1] = 1;
+			boolean b1 = p1 > 0;
+			boolean b2 = p2 > 0;
+			if (b1 ^ b2) {
+				for (int j = base; j <= i; j++) {
+					int value = output[j];
+					output[j] = value + 1;
+				}
+			} // end of if
+			else {
+				base = i + 1;
+			} // end of else
+		} // end of for loop
+		return convertToString(output);
+	}
+
+	private static String convertToString(int[] output) {
+		StringBuilder sb = new StringBuilder();
+		int lenght = output.length;
+		for (int i = 0; i < lenght; i++) {
+			sb.append(output[i]);
+			if (i != lenght - 1) {
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
+	}
+
+	static int[] getArrayOfString(String input) {
+		String[] arrayStr = input.split(" ");
+		int lenght = arrayStr.length;
+		int[] returnArray = new int[lenght];
+		for (int i = 0; i < lenght; i++) {
+			returnArray[i] = Integer.parseInt(arrayStr[i]);
+		}
+		return returnArray;
+	}
+
+	private static InputStream createInputStream() throws FileNotFoundException {
+		if (isCodechefModeOn) {
+			return System.in;
+		}
+		String path = System.getProperty("user.dir");
+		InputStream in = new FileInputStream(path + "/ALTARAY.txt");
+		return in;
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private static String readLine(DataInputStream reader) throws IOException {
+		return reader.readLine();
+	}
+}
---
+++ src/test/java/org/muzir/codechef/practice/beginner/ALTARAYTest.java
@@ -0,0 +1,78 @@
+package org.muzir.codechef.practice.beginner;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ALTARAYTest {
+
+	@Test
+	public void test() {
+		int[] a = { 1, 2, 3, 4 };
+		assertEquals("1 1 1 1", ALTARAY.solution(a));
+	}
+
+	@Test
+	public void test1() {
+		int[] a = { 1, -5, 1, -5 };
+		assertEquals("4 3 2 1", ALTARAY.solution(a));
+	}
+
+	@Test
+	public void test2() {
+		int[] a = { -5, -1, -1, 2, -2, -3 };
+		assertEquals("1 1 3 2 1 1", ALTARAY.solution(a));
+	}
+
+	@Test
+	public void test3() {
+		int[] a = { -10, 1, -10, 1, -10, 1, -10, 1, -10, 1, -10, 1 };
+		assertEquals("12 11 10 9 8 7 6 5 4 3 2 1", ALTARAY.solution(a));
+	}
+
+	@Test
+	public void test4() {
+		int[] a = new int[100000];
+		for (int i = 0; i < 100000; i++) {
+			if (i % 2 == 0) {
+				a[i] = -1;
+			} else {
+				a[i] = 1;
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 100000; 0 < i; i--) {
+			sb.append(i);
+			if (i != 1) {
+				sb.append(" ");
+			}
+		}
+		long first = System.currentTimeMillis();
+		assertEquals(sb.toString(), ALTARAY.solution(a));
+		long second = System.currentTimeMillis();
+		System.out.println("Milis:" + (second - first));
+	}
+
+	@Test
+	public void test5() {
+		int[] a = new int[100000];
+		for (int i = 0; i < 100000; i++) {
+			if (i % 2 == 0) {
+				a[i] = -1;
+			} else {
+				a[i] = 1;
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 100000; 0 < i; i--) {
+			sb.append(i);
+			if (i != 1) {
+				sb.append(" ");
+			}
+		}
+		long first = System.currentTimeMillis();
+		assertEquals(sb.toString(), ALTARAY1.solution(a));
+		long second = System.currentTimeMillis();
+		System.out.println("Milis:" + (second - first));
+	}
+}
