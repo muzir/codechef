aa122d4ef7618417b013590fc4fb84e4f5f57110
muzir <baycelik_erhun@hotmail.com>
2017-02-18 15:32:51 +0200

    NOTINCOM is solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/NOTINCOM.java
@@ -0,0 +1,75 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class NOTINCOM {
+	private static boolean isCodechefModeOn = false;
+
+	public static void main(String[] args) throws IOException {
+		InputStream in = createInputStream();
+		BufferedInputStream reader = new BufferedInputStream(in);
+		DataInputStream dis = new DataInputStream(reader);
+		int testCaseCount = Integer.parseInt(readLine(dis));
+		for (int i = 0; i < testCaseCount; i++) {
+			readLine(dis);
+			String aline = readLine(dis);
+			int[] a = getArrayOfString(aline);
+            String bline = readLine(dis);
+			int[] b = getArrayOfString(bline);
+			System.out.println(solution(a,b));
+		}
+		dis.close();
+	}
+
+	static int solution(int[] alice,int[] berta) {
+		int alenght = alice.length;
+        int blenght=berta.length;
+        int counter=0;
+        int[] all=new int[1000000];
+		for(int i=0;i<alenght;i++){
+            int v=alice[i];
+            int index=v-1;
+            all[index]=v;
+        }
+        for(int i=0;i<blenght;i++){
+            int v=berta[i];
+            int index=v-1;
+            if(all[index]!=0){
+                counter=counter+1;
+            }
+            all[index]=v;
+        }
+		return counter;
+	}
+
+
+	static int[] getArrayOfString(String input) {
+		String[] arrayStr = input.split(" ");
+		int lenght = arrayStr.length;
+		int[] returnArray = new int[lenght];
+		for (int i = 0; i < lenght; i++) {
+			returnArray[i] = Integer.parseInt(arrayStr[i]);
+		}
+		return returnArray;
+	}
+
+	private static InputStream createInputStream() throws FileNotFoundException {
+		if (isCodechefModeOn) {
+			return System.in;
+		}
+		String path = System.getProperty("user.dir");
+		InputStream in = new FileInputStream(path + "/NOTINCOM.txt");
+		return in;
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private static String readLine(DataInputStream reader) throws IOException {
+		return reader.readLine();
+	}
+}
