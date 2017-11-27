6e98b5620dc5bda46bbae55b4127ae73ad568fa5
Erhun Baycelik <erhun.baycelik@transferwise.com>
2017-11-27 23:48:31 +0200

    -- ICPC16B is solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/ICPC16B.java
@@ -0,0 +1,79 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.*;
+
+public class ICPC16B {
+	private static boolean isCodechefModeOn = false;
+	private final static String YES = "yes";
+	private final static String NO = "no";
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			String n = br.readLine();
+			String series = br.readLine();
+			System.out.println(checkBeautifulArray(series));
+		}
+		br.close();
+	}
+
+	static String checkBeautifulArray(String series) {
+		String[] array = series.split(" ");
+		int positiveCounter = 0;
+		int negativeCounter = 0;
+		int minusOneCounter = 0;
+		int oneCounter = 0;
+		int lenght = array.length;
+		if (lenght == 1) {
+			return YES;
+		}
+		for (int i = 0; i < lenght; i++) {
+			String param = array[i];
+			int intParam = Integer.parseInt(param);
+			if (intParam == -1) {
+				minusOneCounter++;
+			} else if (intParam == 1) {
+				oneCounter++;
+			} else if (intParam > 1) {
+				positiveCounter++;
+				if (positiveCounter > 1) {
+					return NO;
+				}
+			} else if (-1 > intParam) {
+				negativeCounter++;
+				if (negativeCounter > 1) {
+					return NO;
+				}
+			}
+			if (minusOneCounter > 0 && negativeCounter > 0) {
+				return NO;
+			}
+			if (minusOneCounter > 0 && positiveCounter > 0) {
+				return NO;
+			}
+			if (negativeCounter > 0 && positiveCounter > 0) {
+				return NO;
+			}
+		}
+		if (minusOneCounter > 1 && oneCounter == 0) {
+			return NO;
+		}
+		return YES;
+	}
+
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/ICPC16B.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+}
---
+++ src/test/java/org/muzir/codechef/practice/beginner/ICPC16BTest.java
@@ -0,0 +1,36 @@
+package org.muzir.codechef.practice.beginner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ICPC16BTest {
+	@Test
+	public void initialCases() {
+		assertEquals("no", ICPC16B.checkBeautifulArray("-1 -1 0"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("1 2 2"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 1 0"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 1 0 -2"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("1 -1 0 -2"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("1 1 0 -2 -2"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 1 1 1"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 1 -1 1"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("12323"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("-1 -1 -1 1"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("-1 -1 -1 0"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("-1 0 0 0"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 0 30"));
+
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 0 -30"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("1 0 -30 -1"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("1 0 -30 -30 -30 -20"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("1 0 1 0 30 1 1 1 0 -2"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("-1 -1 0 1"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("-1 -1 -1"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("0 0 0 0 0 0 0 0"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0"));
+		assertEquals("no", ICPC16B.checkBeautifulArray("-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 2"));
+		assertEquals("yes", ICPC16B.checkBeautifulArray("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 10"));
+	}
+}
