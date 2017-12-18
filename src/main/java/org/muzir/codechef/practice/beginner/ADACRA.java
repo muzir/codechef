43a7d1f031da40617569718b0f2bb94fdeb769cf
Erhun Baycelik <erhun.baycelik@transferwise.com>
2017-12-18 23:46:10 +0200

    ADACRA is solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/ADACRA.java
@@ -0,0 +1,58 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.*;
+
+public class ADACRA {
+	private static boolean isCodechefModeOn = false;
+	static final char U = 'U';
+	static final char D = 'D';
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			System.out.println(calculateMinimumStep(br.readLine()));
+		}
+		br.close();
+	}
+
+	static int calculateMinimumStep(String line) {
+		int length = line.length();
+		char prev = line.charAt(0);
+		int dCounter = 0;
+		int uCounter = 0;
+		for (int i = 1; i < length; i++) {
+			char current = line.charAt(i);
+			if (prev != current) {
+				if (current == U) {
+					dCounter++;
+				}
+				if (current == D) {
+					uCounter++;
+				}
+			}
+			prev = current;
+		}
+		char lastChar = line.charAt(length - 1);
+		if (lastChar == U) {
+			uCounter++;
+		} else {
+			dCounter++;
+		}
+		return Math.min(dCounter, uCounter);
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/ADACRA.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+}
---
+++ src/test/java/org/muzir/codechef/practice/beginner/ADACRATest.java
@@ -0,0 +1,19 @@
+package org.muzir.codechef.practice.beginner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ADACRATest {
+	@Test
+	public void initialCases() {
+		assertEquals(1, ADACRA.calculateMinimumStep("UUDDDUUU"));
+		assertEquals(0, ADACRA.calculateMinimumStep("UUUUU"));
+		assertEquals(0, ADACRA.calculateMinimumStep("DDDDD"));
+		assertEquals(0, ADACRA.calculateMinimumStep("U"));
+		assertEquals(0, ADACRA.calculateMinimumStep("D"));
+		assertEquals(2, ADACRA.calculateMinimumStep("UUDDDUUUD"));
+		assertEquals(2, ADACRA.calculateMinimumStep("UUDDDUUUDU"));
+		assertEquals(6, ADACRA.calculateMinimumStep("UDUDUDUDUDUDU"));
+	}
+}
