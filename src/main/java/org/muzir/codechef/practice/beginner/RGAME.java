4d72a79f971838f78f3038dc29eee731d1d55810
Erhun Baycelik <erhun.baycelik@transferwise.com>
2018-01-17 22:54:26 +0200

    -- RGAME is not solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/RGAME.java
@@ -0,0 +1,30 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.*;
+
+public class RGAME {
+	private static boolean isCodechefModeOn = false;
+
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = createInputStream();
+			int testCaseCount = Integer.parseInt(br.readLine());
+			for (int i = 0; i < testCaseCount; i++) {
+				System.out.println();
+			}
+			br.close();
+		}
+
+		private static BufferedReader createInputStream() throws FileNotFoundException {
+			InputStreamReader isr = null;
+			if (isCodechefModeOn) {
+				isr = new InputStreamReader(System.in);
+			} else {
+				String path = System.getProperty("user.dir");
+				String filePath = path + "/RGAME.txt";
+				FileInputStream fis = new FileInputStream(filePath);
+				isr = new InputStreamReader(fis);
+			}
+			BufferedReader br = new BufferedReader(isr);
+			return br;
+		}
+}
