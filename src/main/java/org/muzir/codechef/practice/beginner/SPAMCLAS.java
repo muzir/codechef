83f6d566efbaf918701d21c31fffa5b3a6ca09fd
Erhun Baycelik <erhun.baycelik@transferwise.com>
2018-01-28 23:33:45 +0200

    -- Practice; SPAMCLAS is solved.

--- src/main/java/org/muzir/codechef/practice/beginner/SPAMCLAS.java
+++ src/main/java/org/muzir/codechef/practice/beginner/SPAMCLAS.java
@@ -31,39 +31,51 @@ public class SPAMCLAS {
 		br.close();
 	}
 
-	private static BufferedReader createInputStream() throws FileNotFoundException {
-		InputStreamReader isr = null;
-		if (isCodechefModeOn) {
-			isr = new InputStreamReader(System.in);
-		} else {
-			String path = System.getProperty("user.dir");
-			String filePath = path + "/SPAMCLAS.txt";
-			FileInputStream fis = new FileInputStream(filePath);
-			isr = new InputStreamReader(fis);
-		}
-		BufferedReader br = new BufferedReader(isr);
-		return br;
-	}
-
 	static String calculateSpamNumbers(int minX, int maxX, int[] ws, int[] bs) {
 		int spammerCounter = 0;
 		int nonSpammerCounter = 0;
 		int lenght = ws.length;
-		boolean isXEven = isEven(minX);
-		for (int i = 0; i < lenght; i++) {
-			if (isEven(isXEven, ws[i], bs[i])) {
-				nonSpammerCounter++;
-				isXEven = true;
-			} else {
-				spammerCounter++;
-				isXEven = false;
-			}
-		}
+		boolean isXEven = isEven(minX, ws, bs, lenght);
+		boolean isXNextEven = isEven(minX + 1, ws, bs, lenght);
 		int total = (maxX - minX) + 1;
+
+		if (isXEven && isXNextEven) {
+			nonSpammerCounter = total;
+		} else if (!isXEven && !isXNextEven) {
+			spammerCounter = total;
+		} else if (isXEven && !isXNextEven) {
+			boolean isTotalEven = isEven(total);
+			if (isTotalEven) {
+				nonSpammerCounter = total / 2;
+			} else {
+				nonSpammerCounter = (total / 2) + 1;
+			}
+			spammerCounter = total / 2;
+		} else {
+			boolean isTotalEven = isEven(total);
+			if (isTotalEven) {
+				spammerCounter = total / 2;
+			} else {
+				spammerCounter = (total / 2) + 1;
+			}
+			nonSpammerCounter = total / 2;
+		}
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(nonSpammerCounter).append(" ").append(spammerCounter);
 		return sb.toString();
+	}
+
+	private static boolean isEven(int minX, int[] ws, int[] bs, int lenght) {
+		boolean isXEven = isEven(minX);
+		for (int i = 0; i < lenght; i++) {
+			if (isEven(isXEven, ws[i], bs[i])) {
+				isXEven = true;
+			} else {
+				isXEven = false;
+			}
+		}
+		return isXEven;
 	}
 
 	static boolean isEven(boolean isXEven, int w, int b) {
@@ -81,5 +93,19 @@ public class SPAMCLAS {
 
 	private static boolean isEven(int i) {
 		return (i % 2) == 0;
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/SPAMCLAS.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
 	}
 }
--- src/test/java/org/muzir/codechef/practice/beginner/SPAMCLASTest.java
+++ src/test/java/org/muzir/codechef/practice/beginner/SPAMCLASTest.java
@@ -8,6 +8,20 @@ public class SPAMCLASTest {
 	public void initialCases() {
 		int[] ws = {1};
 		int[] bs = {2};
-		Assert.assertEquals("1 2",SPAMCLAS.calculateSpamNumbers(1, 2, ws, bs));
+		Assert.assertEquals("1 1", SPAMCLAS.calculateSpamNumbers(1, 2, ws, bs));
+	}
+
+	@Test
+	public void initialCases_1() {
+		int[] ws = {2, 2};
+		int[] bs = {4, 3};
+		Assert.assertEquals("0 4", SPAMCLAS.calculateSpamNumbers(1, 4, ws, bs));
+	}
+
+	@Test
+	public void initialCases_2() {
+		int[] ws = {2, 2, 5};
+		int[] bs = {4, 2, 4};
+		Assert.assertEquals("999999999 0", SPAMCLAS.calculateSpamNumbers(2, 1000000000, ws, bs));
 	}
 }
