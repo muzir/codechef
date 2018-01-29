71be8132af72e20565c5d4085c643840e366a42a
Erhun Baycelik <erhun.baycelik@transferwise.com>
2018-01-29 23:56:03 +0200

    -- Practice; RGAME is not solved.

--- src/main/java/org/muzir/codechef/practice/beginner/RGAME.java
+++ src/main/java/org/muzir/codechef/practice/beginner/RGAME.java
@@ -1,30 +1,39 @@
 package org.muzir.codechef.practice.beginner;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class RGAME {
 	private static boolean isCodechefModeOn = false;
 
-		public static void main(String[] args) throws IOException {
-			BufferedReader br = createInputStream();
-			int testCaseCount = Integer.parseInt(br.readLine());
-			for (int i = 0; i < testCaseCount; i++) {
-				System.out.println();
-			}
-			br.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			br.readLine();
+			int[] numbers = Arrays.stream(br.readLine().split(" "))
+					.mapToInt(Integer::parseInt)
+					.toArray();
+			System.out.println(calculateGameScore(numbers));
 		}
+		br.close();
+	}
 
-		private static BufferedReader createInputStream() throws FileNotFoundException {
-			InputStreamReader isr = null;
-			if (isCodechefModeOn) {
-				isr = new InputStreamReader(System.in);
-			} else {
-				String path = System.getProperty("user.dir");
-				String filePath = path + "/RGAME.txt";
-				FileInputStream fis = new FileInputStream(filePath);
-				isr = new InputStreamReader(fis);
-			}
-			BufferedReader br = new BufferedReader(isr);
-			return br;
+	static int calculateGameScore(int[] numbers) {
+		return 0;
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/RGAME.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
 		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
 }
