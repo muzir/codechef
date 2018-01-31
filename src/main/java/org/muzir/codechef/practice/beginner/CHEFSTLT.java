732572964756baf77e537f805db67c2c2e5ac47f
Erhun Baycelik <erhun.baycelik@transferwise.com>
2018-01-31 21:53:27 +0200

    -- CHEFSTLT solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/CHEFSTLT.java
@@ -0,0 +1,70 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.*;
+
+public class CHEFSTLT {
+
+	private static boolean isCodechefModeOn = false;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			String param1 = br.readLine();
+			String param2 = br.readLine();
+			System.out.println(calculateDifference(param1, param2));
+		}
+		br.close();
+	}
+
+	static String calculateDifference(String param1, String param2) {
+		Difference d = new Difference();
+		char[] chars1 = param1.toCharArray();
+		char[] chars2 = param2.toCharArray();
+		int length = chars1.length;
+		for (int i = 0; i < length; i++) {
+			char c1 = chars1[i];
+			char c2 = chars2[i];
+			if (c1 == '?' || c2 == '?') {
+				d.increaseMaxDifference();
+
+			} else if (c1 != c2) {
+				d.increaseMaxDifference();
+				d.increaseMinDifference();
+			}
+		}
+		return d.toString();
+	}
+
+	static class Difference {
+		private int minDifference;
+		private int maxDifference;
+
+		void increaseMinDifference() {
+			minDifference++;
+		}
+
+		void increaseMaxDifference() {
+			maxDifference++;
+		}
+
+		@Override
+		public String toString() {
+			return minDifference + " " + maxDifference;
+		}
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/CHEFSTLT.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+}
--- src/test/java/org/muzir/codechef/practice/beginner/CHEFSTLTTest.java
+++ src/test/java/org/muzir/codechef/practice/beginner/CHEFSTLTTest.java
@@ -1,45 +1,34 @@
 package org.muzir.codechef.practice.beginner;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class CHEFSTLTTest {
-	private Chefstlt chefstlt;
-
-	@Before
-	public void setUp() {
-		chefstlt = new Chefstlt();
-	}
 
 	@Test
 	public void testIsAllowedCharacter() {
-		Assert.assertEquals("1 3", chefstlt.calculateDifference("a?c", "??b"));
-		Assert.assertEquals("0 3", chefstlt.calculateDifference("???a", "???a"));
-		Assert.assertEquals("3 5", chefstlt.calculateDifference("?abac", "aba?w"));
+		Assert.assertEquals("1 3", CHEFSTLT.calculateDifference("a?c", "??b"));
+		Assert.assertEquals("0 3", CHEFSTLT.calculateDifference("???a", "???a"));
+		Assert.assertEquals("3 5", CHEFSTLT.calculateDifference("?abac", "aba?w"));
 	}
 
 	@Test
 	public void testCornerCases() {
-		Assert.assertEquals("0 0", chefstlt.calculateDifference("", ""));
-		Assert.assertEquals("0 0", chefstlt.calculateDifference(null, null));
-		Assert.assertEquals("0 0", chefstlt.calculateDifference(null, "dsada"));
-		Assert.assertEquals("0 0", chefstlt.calculateDifference("dadas", null));
-		Assert.assertEquals("1 1", chefstlt.calculateDifference("0", "1"));
-		Assert.assertEquals("0 0", chefstlt.calculateDifference("0", "0"));
-		Assert.assertEquals("0 1", chefstlt.calculateDifference("a", "?"));
-		Assert.assertEquals("0 1", chefstlt.calculateDifference("?", "a"));
-		Assert.assertEquals("0 1", chefstlt.calculateDifference("?", "?"));
-		Assert.assertEquals("0 2", chefstlt.calculateDifference("??", "??"));
-		Assert.assertEquals("0 3", chefstlt.calculateDifference("???", "???"));
+		Assert.assertEquals("1 1", CHEFSTLT.calculateDifference("0", "1"));
+		Assert.assertEquals("0 0", CHEFSTLT.calculateDifference("0", "0"));
+		Assert.assertEquals("0 1", CHEFSTLT.calculateDifference("a", "?"));
+		Assert.assertEquals("0 1", CHEFSTLT.calculateDifference("?", "a"));
+		Assert.assertEquals("0 1", CHEFSTLT.calculateDifference("?", "?"));
+		Assert.assertEquals("0 2", CHEFSTLT.calculateDifference("??", "??"));
+		Assert.assertEquals("0 3", CHEFSTLT.calculateDifference("???", "???"));
 	}
 
 	@Test
-	public void testLargeInputs(){
-		Assert.assertEquals("1 12", chefstlt.calculateDifference("abcd?e?f?g?i?", "aa?????????????"));
-		Assert.assertEquals("2 8", chefstlt.calculateDifference("???FaaRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???"));
-		Assert.assertEquals("16 22", chefstlt.calculateDifference("???FaaRDbIGCWlIRXf4NuHblfE4vx44neeeeeeeewpPXVIPOWliljPuf2w3VEhJveeW4bBeeeeee5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???"));
-		Assert.assertEquals("70 76", chefstlt.calculateDifference("???FaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaCa5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???"));
-		Assert.assertEquals("70 82", chefstlt.calculateDifference("???FaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaCa5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fab?????????"));
+	public void testLargeInputs() {
+		Assert.assertEquals("1 12", CHEFSTLT.calculateDifference("abcd?e?f?g?i?", "aa?????????????"));
+		Assert.assertEquals("2 8", CHEFSTLT.calculateDifference("???FaaRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???"));
+		Assert.assertEquals("16 22", CHEFSTLT.calculateDifference("???FaaRDbIGCWlIRXf4NuHblfE4vx44neeeeeeeewpPXVIPOWliljPuf2w3VEhJveeW4bBeeeeee5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???"));
+		Assert.assertEquals("70 76", CHEFSTLT.calculateDifference("???FaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaCa5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fabk0aZmX???"));
+		Assert.assertEquals("70 82", CHEFSTLT.calculateDifference("???FaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaCa5HiUrga2ngt6LexZG9Fabk0aZmX???", "???FuJRDbIGCWlIRXf4NuHblfE4vx44nsljVg7xMwpPXVIPOWliljPuf2w3VEhJveeW4bBNmLtCa5HiUrga2ngt6LexZG9Fab?????????"));
 	}
 }
