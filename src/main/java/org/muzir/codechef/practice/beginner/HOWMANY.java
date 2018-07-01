e87688f70643ee205e2579de4205a94941f2e165
Erhun Baycelik <erhun.baycelik@transferwise.com>
2018-07-01 18:52:23 +0300

    HOWMANY is solved

--- src/main/java/org/muzir/codechef/practice/beginner/HOWMANY.java
+++ src/main/java/org/muzir/codechef/practice/beginner/HOWMANY.java
@@ -1,5 +1,46 @@
 package org.muzir.codechef.practice.beginner;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HOWMANY {
 
+    private static boolean isCodechefModeOn = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = createInputStream();
+        String param = br.readLine();
+        solution(param);
+        br.close();
+    }
+
+    public static void solution(String param) {
+        int l = param.length();
+        if (l == 1) {
+            System.out.println("1");
+        } else if (l == 2) {
+            System.out.println("2");
+        } else if (l == 3) {
+            System.out.println("3");
+        } else {
+            System.out.println("More than 3 digits");
+        }
+    }
+
+    private static BufferedReader createInputStream() throws FileNotFoundException {
+        InputStreamReader isr = null;
+        if (isCodechefModeOn) {
+            isr = new InputStreamReader(System.in);
+        } else {
+            String path = System.getProperty("user.dir");
+            String filePath = path + "/HOWMANY.txt";
+            FileInputStream fis = new FileInputStream(filePath);
+            isr = new InputStreamReader(fis);
+        }
+        BufferedReader br = new BufferedReader(isr);
+        return br;
+    }
 }
--- src/test/java/org/muzir/codechef/longContest/may18/RD19Test.java
+++ src/test/java/org/muzir/codechef/longContest/may18/RD19Test.java
@@ -1,4 +1,60 @@
 package org.muzir.codechef.longContest.may18;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RD19Test {
+	@Test
+	public void test() {
+		int[] parameters = {2, 4, 6, 8};
+		Assert.assertEquals(-1, RD19.solution(parameters));
+	}
+
+	@Test
+	public void test1() {
+		int[] parameters = {2, 4, 6, 9};
+		Assert.assertEquals(0, RD19.solution(parameters));
+	}
+
+	@Test
+	public void test2() {
+		int[] parameters = {2, 3};
+		Assert.assertEquals(0, RD19.solution(parameters));
+	}
+
+	@Test
+	public void test3() {
+		int[] parameters = {23, 23};
+		Assert.assertEquals(-1, RD19.solution(parameters));
+	}
+
+	@Test
+	public void test4() {
+		int[] parameters = {23, 23};
+		Assert.assertEquals(-1, RD19.solution(parameters));
+	}
+
+	@Test
+	public void test5() {
+		int i, k, j;
+		int count = 0;
+		StringBuilder sb = new StringBuilder();
+		for (i = 2; i <= 25000; i++) {
+			k = 0;
+			for (j = 2; j < i; j++) {
+				if (i % j == 0) {
+					k = 1;
+					break;
+				}
+			}
+			if (k == 0) {
+				sb.append(i).append(",");
+				count++;
+			}
+		}
+		count--;
+		System.out.println("Total: " + count);
+		System.out.println(sb.toString());
+	}
+
 }
