9e1d925393a152153dbbd8f2f70d5d961222cc4a
Erhun Baycelik <erhun.baycelik@transferwise.com>
2018-07-16 00:26:30 +0300

    -- NUMCOMP is solved.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/NUMCOMP.java
@@ -0,0 +1,90 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class NUMCOMP {
+
+    private static boolean isCodechefModeOn = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = createInputStream();
+        int testCaseCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCaseCount; i++) {
+            String abn = br.readLine();
+            int[] params = Arrays.stream(abn.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+            System.out.println(solution(params[0], params[1], params[2]));
+        }
+        br.close();
+    }
+
+
+    static int solution(int a, int b, int n) {
+        boolean isAPositive = isPositive(a);
+        boolean isBPositive = isPositive(b);
+        boolean isNEven = isEven(n);
+
+        if (a == b) {
+            return 0;
+        } else if (isNEven & (Math.abs(a) == Math.abs(b))) {
+            return 0;
+        }
+
+        boolean result = false;
+        if (isAPositive && isBPositive) {
+            result = compare(a, b);
+        } else if (!isAPositive && isBPositive && !isNEven) {
+            result = false;
+        } else if (!isAPositive && isBPositive && isNEven) {
+            result = compare(Math.abs(a), Math.abs(b));
+        } else if (!isAPositive && !isBPositive && !isNEven) {
+            result = !compare(Math.abs(a), Math.abs(b));
+        }else if (!isAPositive && !isBPositive && isNEven) {
+            result = compare(Math.abs(a), Math.abs(b));
+        }else if (isAPositive && !isBPositive && isNEven) {
+            result = compare(Math.abs(a), Math.abs(b));
+        }
+        else {
+            result = compare(a, b);
+        }
+
+        if (result) {
+            return 1;
+        }
+        return 2;
+
+    }
+
+    static boolean isEven(int n) {
+        return (n % 2) == 0;
+    }
+
+    static boolean isPositive(int n) {
+        return n > 0;
+    }
+
+    static boolean compare(int a, int b) {
+        return a > b;
+    }
+
+    private static BufferedReader createInputStream() throws FileNotFoundException {
+        InputStreamReader isr = null;
+        if (isCodechefModeOn) {
+            isr = new InputStreamReader(System.in);
+        } else {
+            String path = System.getProperty("user.dir");
+            String filePath = path + "/NUMCOMP.txt";
+            FileInputStream fis = new FileInputStream(filePath);
+            isr = new InputStreamReader(fis);
+        }
+        BufferedReader br = new BufferedReader(isr);
+        return br;
+    }
+
+}
---
+++ src/test/java/org/muzir/codechef/practice/beginner/NUMCOMPTest.java
@@ -0,0 +1,63 @@
+package org.muzir.codechef.practice.beginner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NUMCOMPTest {
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(NUMCOMP.solution(3, 4, 5), 2);
+        Assert.assertEquals(NUMCOMP.solution(3, 4, 4), 2);
+        Assert.assertEquals(NUMCOMP.solution(-3, 2, 3), 2);
+        Assert.assertEquals(NUMCOMP.solution(-3, 2, 4), 1);
+        Assert.assertEquals(NUMCOMP.solution(-2, 3, 5), 2);
+        Assert.assertEquals(NUMCOMP.solution(-2, 3, 4), 2);
+        Assert.assertEquals(NUMCOMP.solution(3, -2, 4), 1);
+        Assert.assertEquals(NUMCOMP.solution(3, -2, 3), 1);
+        Assert.assertEquals(NUMCOMP.solution(-3, -3, 3), 0);
+        Assert.assertEquals(NUMCOMP.solution(3, 3, 3), 0);
+        Assert.assertEquals(NUMCOMP.solution(-3, -8, 2), 2);
+        Assert.assertEquals(NUMCOMP.solution(-9, -8, 2), 1);
+        Assert.assertEquals(NUMCOMP.solution(-9, 8, 2), 1);
+        Assert.assertEquals(NUMCOMP.solution(-9, 8, 3), 2);
+        Assert.assertEquals(NUMCOMP.solution(0, 8, 3), 2);
+        Assert.assertEquals(NUMCOMP.solution(0, -8, 3), 1);
+        Assert.assertEquals(NUMCOMP.solution(0, -8, 4), 2);
+        Assert.assertEquals(NUMCOMP.solution(0, 0, 4), 0);
+        Assert.assertEquals(NUMCOMP.solution(-1, 0, 3), 2);
+        Assert.assertEquals(NUMCOMP.solution(-1, 0, 4), 1);
+        Assert.assertEquals(NUMCOMP.solution(-1, 0, 1), 2);
+        Assert.assertEquals(NUMCOMP.solution(2, -3, 4), 2);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(NUMCOMP.solution(-334467, 334467, 2348876), 0);
+        Assert.assertEquals(NUMCOMP.solution(334467, -334467, 2348876), 0);
+        Assert.assertEquals(NUMCOMP.solution(-334467, 334467, 2348877), 2);
+        Assert.assertEquals(NUMCOMP.solution(334467, -334467, 2348877), 1);
+        Assert.assertEquals(NUMCOMP.solution(363756450, 712662868, 24979445), 2);
+        Assert.assertEquals(NUMCOMP.solution(586260100, -192321079, 476190629), 1);
+        Assert.assertEquals(NUMCOMP.solution(-10, 18, 628894325), 2);
+        Assert.assertEquals(NUMCOMP.solution(-354684803, -49687658, 430302156), 1);
+        Assert.assertEquals(NUMCOMP.solution(853829789, 470958158, 923078537), 1);
+        Assert.assertEquals(NUMCOMP.solution(-16, -10, 802329211), 2);
+        Assert.assertEquals(NUMCOMP.solution(-19, -20, 492686568), 2);
+        Assert.assertEquals(NUMCOMP.solution(23704508, 150490716, 839296263), 2);
+
+        Assert.assertEquals(NUMCOMP.solution(14, 8, 191890310), 1);
+        Assert.assertEquals(NUMCOMP.solution(-20, -13, 818008580), 1);
+       // Assert.assertEquals(NUMCOMP.solution(2, -15, 840594328), 2);
+        Assert.assertEquals(NUMCOMP.solution(301010289, 367300984, 501899080), 2);
+        Assert.assertEquals(NUMCOMP.solution(949600446, 943054987, 350034067), 1);
+        Assert.assertEquals(NUMCOMP.solution(16, 10, 533206504), 1);
+        Assert.assertEquals(NUMCOMP.solution(8, 7, 677959980), 1);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals(NUMCOMP.solution(2, -15, 840594328), 2);
+    }
+
+}
