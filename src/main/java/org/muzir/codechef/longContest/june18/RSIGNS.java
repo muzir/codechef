badea9451567e27a86e91d75e1926b4214ec0576
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-06-08 19:21:54 +0300

    -- RSIGNS is solved.

---
+++ src/main/java/org/muzir/codechef/longContest/june18/RSIGNS.java
@@ -0,0 +1,72 @@
+package org.muzir.codechef.longContest.june18;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class RSIGNS {
+	static final int MODULO_CONSTANT = 1_000_000_007;
+	private static boolean isCodechefModeOn = false;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			int k = Integer.parseInt(br.readLine());
+			System.out.println(computeViaFormula(k));
+		}
+		br.close();
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/src/main/inputs/RSIGNS.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+
+	public static long computeViaFormula(int k) {
+		BigInteger modConstant = BigInteger.valueOf(MODULO_CONSTANT);
+		BigInteger result = new BigInteger("2").modPow(BigInteger.valueOf(k - 1), modConstant).multiply(BigInteger.TEN).mod(modConstant);
+		return result.longValue();
+	}
+
+
+	/*public static long solution(int k) {
+		long l = ((long) Math.pow(10, k)) - 1l;
+		long counter = 0;
+		for (long i = 0; i <= l; i++) {
+			long l1 = l - i;
+			if (isTwoDistinctDigit(l1, i)) {
+				counter = counter + 1;
+			}
+		}
+		return counter;
+	}*/
+
+	/*private static boolean isTwoDistinctDigit(long param, long param1) {
+		String s1 = String.valueOf(param);
+		String s2 = String.valueOf(param1);
+
+		Map<Character, Integer> charMaps = new HashMap<>();
+
+		char[] charArray1 = s1.toCharArray();
+		for (char c : charArray1) {
+			charMaps.put(Character.valueOf(c), 1);
+		}
+
+		char[] charArray2 = s2.toCharArray();
+		for (char c : charArray2) {
+			charMaps.put(Character.valueOf(c), 1);
+		}
+
+		return charMaps.size() == 2;
+	}*/
+
+}
---
+++ src/test/java/org/muzir/codechef/longContest/june18/RSIGNSTest.java
@@ -0,0 +1,56 @@
+package org.muzir.codechef.longContest.june18;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.muzir.codechef.longContest.june18.RSIGNS.MODULO_CONSTANT;
+
+public class RSIGNSTest {
+
+	@Test
+	public void test3() {
+		Assert.assertEquals(10l, RSIGNS.computeViaFormula(1));
+		Assert.assertEquals(20l, RSIGNS.computeViaFormula(2));
+		Assert.assertEquals(40l, RSIGNS.computeViaFormula(3));
+		Assert.assertEquals(80l, RSIGNS.computeViaFormula(4));
+		Assert.assertEquals(160l, RSIGNS.computeViaFormula(5));
+		Assert.assertEquals(320l, RSIGNS.computeViaFormula(6));
+		Assert.assertEquals(640l, RSIGNS.computeViaFormula(7));
+		Assert.assertEquals(1280l, RSIGNS.computeViaFormula(8));
+		Assert.assertEquals(2560l, RSIGNS.computeViaFormula(9));
+		Assert.assertEquals(5120l, RSIGNS.computeViaFormula(10));
+	}
+
+	@Test
+	public void test2() {
+		//https://stackoverflow.com/a/4102123/1577363
+		int k = 1_000_000_000;
+		/*BigDecimal param = BigDecimal.valueOf(2);
+		BigDecimal result = param.pow(k - 1).multiply(BigDecimal.TEN);
+		System.out.println(result.toString());*/
+
+		//BigInteger bi = new BigInteger("1").shiftLeft(k - 1).mod(BigInteger.valueOf(MODULO_CONSTANT));
+		//BigInteger b2 = new BigInteger("1").shiftLeft(k - 1);
+		//b2.mod(BigInteger.valueOf(MODULO_CONSTANT));
+		//BigInteger bi1 = new BigInteger("2").modPow(BigInteger.valueOf(k - 1), BigInteger.valueOf(MODULO_CONSTANT)).multiply(BigInteger.TEN).mod(BigInteger.valueOf(MODULO_CONSTANT));
+		BigInteger bi2 = new BigInteger("2").pow(k - 1).multiply(BigInteger.TEN).mod(BigInteger.valueOf(MODULO_CONSTANT));
+		System.out.println(bi2);
+		//System.out.println(bi);
+	}
+
+	@Test
+	public void test4() {
+		Assert.assertEquals(703125005, RSIGNS.computeViaFormula(1_000_000_000));
+	}
+
+	@Test
+	public void test5() {
+		Assert.assertEquals(881856397, RSIGNS.computeViaFormula(100));
+		Assert.assertEquals(848700849, RSIGNS.computeViaFormula(150));
+		Assert.assertEquals(442116029, RSIGNS.computeViaFormula(1000));
+		Assert.assertEquals(175210288, RSIGNS.computeViaFormula(1000_000));
+		Assert.assertEquals(703125005, RSIGNS.computeViaFormula(1000_000_000));
+	}
+}
