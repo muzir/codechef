94371255d3c6be46be9000a7080ffbb8da0a85fb
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-10-26 10:39:28 +0300

    SAKTAN is partially solved.

--- src/main/java/org/muzir/codechef/longContest/oct19b/SAKTAN.java
+++ src/main/java/org/muzir/codechef/longContest/oct19b/SAKTAN.java
@@ -7,13 +7,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.RecursiveTask;
 
 public class SAKTAN {
+	static HashMap<Key, Integer> cache;
 	private static boolean isCodechefModeOn = false;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = createInputStream();
 		int testCaseCount = Integer.parseInt(br.readLine());
+		cache = new HashMap<>(testCaseCount);
 		for (int i = 0; i < testCaseCount; i++) {
 			int[] params = Arrays.stream(br.readLine().split(" "))
 					.mapToInt(Integer::parseInt)
@@ -48,41 +52,146 @@ public class SAKTAN {
 	public static int solution(int rowCount, int colCount, String[] paramArray) {
 
 		int oddCounter = 0;
+		Key k = Key.createKey(rowCount, colCount, paramArray);
+		if (cache.get(k) != null) {
+			return cache.get(k);
+		}
 
-		HashMap<Integer, Integer> rowHashMap = new HashMap<>(rowCount);
-		HashMap<Integer, Integer> colHashMap = new HashMap<>(colCount);
+		int[] rows = new int[rowCount];
+		int[] columns = new int[colCount];
 
 		Arrays.stream(paramArray).forEach(s -> {
 			String[] params = s.split(" ");
-			int row = Integer.parseInt(params[0]);
-			int col = Integer.parseInt(params[1]);
+			int row = Integer.parseInt(params[0]) - 1;
+			int col = Integer.parseInt(params[1]) - 1;
 
-			int rowSum = getValue(rowHashMap, row) + 1;
-			rowHashMap.put(row, rowSum);
+			int rowSum = rows[row] == 0 ? 1 : 0;
+			rows[row] = rowSum;
 
-			int colSum = getValue(colHashMap, col) + 1;
-			colHashMap.put(col, colSum);
-
+			int colSum = columns[col] == 0 ? 1 : 0;
+			columns[col] = colSum;
 		});
 
-		for (int i = 1; i <= rowCount; i++) {
-			int row = getValue(rowHashMap, i);
-			for (int j = 1; j <= colCount; j++) {
-				int col = getValue(colHashMap, j);
+		for (int i = 0; i < rowCount; i++) {
+			int row = rows[i];
+			for (int j = 0; j < colCount; j++) {
+				int col = columns[j];
 				int cellValue = row + col;
-				if (isOdd(cellValue)) {
+				if (cellValue == 1) {
 					oddCounter = oddCounter + 1;
 				}
 			}
 		}
+
+		cache.put(k, oddCounter);
 		return oddCounter;
 	}
 
-	private static int getValue(HashMap<Integer, Integer> map, int index) {
-		return map.get(index) == null ? 0 : map.get(index);
+	public static int paralelSolution(int rowCount, int colCount, String[] paramArray) {
+
+		int oddCounter = 0;
+		Key k = Key.createKey(rowCount, colCount, paramArray);
+		if (cache.get(k) != null) {
+			return cache.get(k);
+		}
+
+		int[] rows = new int[rowCount];
+		int[] columns = new int[colCount];
+
+		Arrays.stream(paramArray)
+				.forEach(s -> {
+					String[] params = s.split(" ");
+					int row = Integer.parseInt(params[0]) - 1;
+					int col = Integer.parseInt(params[1]) - 1;
+
+					int rowSum = rows[row] == 0 ? 1 : 0;
+					rows[row] = rowSum;
+
+					int colSum = columns[col] == 0 ? 1 : 0;
+					columns[col] = colSum;
+				});
+
+
+		OddCalculator calculator = new OddCalculator(0, rows.length - 1, columns, rows);
+		oddCounter = calculator.compute();
+
+
+		cache.put(k, oddCounter);
+		return oddCounter;
 	}
 
-	private static boolean isOdd(int cellValue) {
-		return cellValue % 2 == 1;
+
+	static class OddCalculator extends RecursiveTask<Integer> {
+		private int startIndex;
+		private int endIndex;
+		private int[] columns;
+		private int[] rows;
+
+		public OddCalculator(int startIndex, int endIndex, int[] columns, int[] rows) {
+			this.startIndex = startIndex;
+			this.endIndex = endIndex;
+			this.columns = columns;
+			this.rows = rows;
+		}
+
+		@Override
+		protected Integer compute() {
+
+			int colCount = columns.length;
+
+			if (startIndex == endIndex) {
+				int oddCounter = 0;
+				int row = rows[startIndex];
+				for (int j = 0; j < colCount; j++) {
+					int col = columns[j];
+					int cellValue = row + col;
+					if (cellValue == 1) {
+						oddCounter = oddCounter + 1;
+					}
+				}
+				return oddCounter;
+			} else if (startIndex > endIndex) {
+				return 0;
+			} else {
+				int mid = startIndex + endIndex / 2;
+				OddCalculator left = new OddCalculator(startIndex, mid, columns, rows);
+				OddCalculator right = new OddCalculator(mid + 1, endIndex, columns, rows);
+				right.fork();
+				return left.compute() + right.join();
+			}
+		}
+	}
+
+	static class Key {
+		int n;
+		int m;
+		String[] paramArray;
+
+		private Key(int n, int m, String[] paramArray) {
+			this.n = n;
+			this.m = m;
+			this.paramArray = paramArray;
+		}
+
+		static Key createKey(int n, int m, String[] paramArray) {
+			return new Key(n, m, paramArray);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Key key = (Key) o;
+			return n == key.n &&
+					m == key.m &&
+					Arrays.equals(paramArray, key.paramArray);
+		}
+
+		@Override
+		public int hashCode() {
+			int result = Objects.hash(n, m);
+			result = 31 * result + Arrays.hashCode(paramArray);
+			return result;
+		}
 	}
 }
--- src/test/java/org/muzir/codechef/longContest/oct19b/SAKTANTest.java
+++ src/test/java/org/muzir/codechef/longContest/oct19b/SAKTANTest.java
@@ -1,10 +1,18 @@
 package org.muzir.codechef.longContest.oct19b;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
 public class SAKTANTest {
+
+	@Before
+	public void setUp() {
+		SAKTAN.cache = new HashMap<>(2);
+	}
 
 	@Test
 	public void test1() {
@@ -26,8 +34,8 @@ public class SAKTANTest {
 	public void test3() {
 		int rowCount = 1;
 		int colCount = 1;
-		String[] paramArray = {"1 0"};
-		assertEquals(1, SAKTAN.solution(rowCount, colCount, paramArray));
+		String[] paramArray = {"1 1"};
+		assertEquals(0, SAKTAN.solution(rowCount, colCount, paramArray));
 	}
 
 	@Test
@@ -36,5 +44,14 @@ public class SAKTANTest {
 		int colCount = 1;
 		String[] paramArray = {"1 1"};
 		assertEquals(0, SAKTAN.solution(rowCount, colCount, paramArray));
+	}
+
+	@Test
+	public void testCache() {
+		int rowCount = 2;
+		int colCount = 2;
+		String[] paramArray = {"1 1", "1 2", "2 1"};
+		assertEquals(2, SAKTAN.solution(rowCount, colCount, paramArray));
+		assertEquals(2, SAKTAN.solution(rowCount, colCount, paramArray));
 	}
 }
