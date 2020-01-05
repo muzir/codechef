e8d2892062bbd21fdf63dabb88a8de7920976378
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-01-05 10:20:54 +0200

    Long contest jan20b.

---
+++ src/main/java/org/muzir/codechef/longContest/jan20b/DYNAMO.java
@@ -0,0 +1,88 @@
+package org.muzir.codechef.longContest.jan20b;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DYNAMO {
+	private static boolean isCodechefModeOn = false;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			int returnCode = solution(br);
+			if (returnCode == -1) {
+				System.exit(returnCode);
+			}
+		}
+		br.close();
+	}
+
+
+	static int solution(BufferedReader br) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+
+		DynamoEngine dynamoEngine = new DynamoEngine(n);
+
+		dynamoEngine.a = Long.parseLong(br.readLine());
+		System.out.println(dynamoEngine.s);
+		dynamoEngine.b = Long.parseLong(br.readLine());
+		System.out.println(dynamoEngine.c);
+		dynamoEngine.d = Long.parseLong(br.readLine());
+		dynamoEngine.calculate();
+		System.out.println(dynamoEngine.e);
+		//System.out.println(dynamoEngine.toString());
+		return Integer.parseInt(br.readLine());
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/src/main/inputs/DYNAMO.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+
+	static class DynamoEngine {
+
+		private int n;
+		private long s;
+		private long a;
+		private long b;
+		private long c;
+		private long d;
+		private long e;
+
+		public DynamoEngine(int n) {
+			this.n = n;
+			this.s = 5 * (long) Math.pow(10, n);
+			this.c = 1;
+		}
+
+		void calculate() {
+			this.e = s - (a + b + c + d);
+		}
+
+		@Override
+		public String toString() {
+			return "DynamoEngine{" +
+					"n=" + n +
+					", s=" + s +
+					", a=" + a +
+					", b=" + b +
+					", c=" + c +
+					", d=" + d +
+					", e=" + e +
+					'}';
+		}
+	}
+}
