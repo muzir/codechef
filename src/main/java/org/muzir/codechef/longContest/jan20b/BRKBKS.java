f300f8df18cea83408d731c4561c658be58f27dd
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-01-05 08:36:54 +0200

    Additional test cases for problem BRKBKS.

--- src/main/java/org/muzir/codechef/longContest/jan20b/BRKBKS.java
+++ src/main/java/org/muzir/codechef/longContest/jan20b/BRKBKS.java
@@ -20,15 +20,30 @@ public class BRKBKS {
 		br.close();
 	}
 
-	 static int solution(String line) {
+	static int solution(String line) {
 		int[] params = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
 		int s = params[0];
+		validateParams(params);
 		int sumOfBricksWidth = 0;
 		for (int i = 1; i < params.length; i++) {
 			sumOfBricksWidth += params[i];
 		}
 		int division = sumOfBricksWidth / s;
+		if (sumOfBricksWidth % s == 0) {
+			return division;
+		}
 		return division + 1;
+	}
+
+	private static void validateParams(int[] params) {
+		int s = params[0];
+		int maxOfTheSequence = 0;
+		for (int i = 1; i < params.length; i++) {
+			maxOfTheSequence = Math.max(maxOfTheSequence, params[i]);
+		}
+		if (maxOfTheSequence > s) {
+			throw new IllegalArgumentException("it is guaranteed that Ada can break all bricks");
+		}
 	}
 
 	private static BufferedReader createInputStream() throws FileNotFoundException {
--- src/test/java/org/muzir/codechef/longContest/jan20b/BRKBKSTest.java
+++ src/test/java/org/muzir/codechef/longContest/jan20b/BRKBKSTest.java
@@ -10,5 +10,40 @@ public class BRKBKSTest {
 		assertEquals(2, BRKBKS.solution("3 1 2 2"));
 		assertEquals(2, BRKBKS.solution("2 1 1 1"));
 		assertEquals(2, BRKBKS.solution("3 2 2 1"));
+		assertEquals(3, BRKBKS.solution("2 2 2 2"));
+		assertEquals(2, BRKBKS.solution("5 2 2 2"));
+		assertEquals(1, BRKBKS.solution("6 2 2 2"));
+		assertEquals(1, BRKBKS.solution("8 2 2 2"));
+		assertEquals(1, BRKBKS.solution("5 2 2 1"));
+		assertEquals(2, BRKBKS.solution("3 1 1 2"));
+		assertEquals(1, BRKBKS.solution("8 2 1 1"));
+	}
+
+	@Test
+	public void test1() {
+		assertEquals(3, BRKBKS.solution("1 1 1 1"));
+		assertEquals(2, BRKBKS.solution("2 1 1 1"));
+		assertEquals(1, BRKBKS.solution("3 1 1 1"));
+		assertEquals(1, BRKBKS.solution("4 1 1 1"));
+		assertEquals(1, BRKBKS.solution("5 1 1 1"));
+		assertEquals(1, BRKBKS.solution("6 1 1 1"));
+		assertEquals(1, BRKBKS.solution("7 1 1 1"));
+		assertEquals(1, BRKBKS.solution("8 1 1 1"));
+	}
+
+	@Test
+	public void test2() {
+		assertEquals(3, BRKBKS.solution("2 2 2 2"));
+		assertEquals(2, BRKBKS.solution("3 2 2 2"));
+		assertEquals(2, BRKBKS.solution("4 2 2 2"));
+		assertEquals(2, BRKBKS.solution("5 2 2 2"));
+		assertEquals(1, BRKBKS.solution("6 2 2 2"));
+		assertEquals(1, BRKBKS.solution("7 2 2 2"));
+		assertEquals(1, BRKBKS.solution("8 2 2 2"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testExceptions() {
+		assertEquals(3, BRKBKS.solution("1 2 2 2"));
 	}
 }
