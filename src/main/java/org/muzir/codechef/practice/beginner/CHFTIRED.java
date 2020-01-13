3583fe7f3180d47e83ebfd4cc0aae4f1a3af3e25
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-01-13 09:21:52 +0200

    CHFTIRED is solved. Remaining 61 questions in beginner.

---
+++ src/main/java/org/muzir/codechef/practice/beginner/CHFTIRED.java
@@ -0,0 +1,35 @@
+package org.muzir.codechef.practice.beginner;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CHFTIRED {
+	private static boolean isCodechefModeOn = false;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = createInputStream();
+		int testCaseCount = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCaseCount; i++) {
+			br.readLine();
+			System.out.println("YES");
+		}
+		br.close();
+	}
+
+	private static BufferedReader createInputStream() throws FileNotFoundException {
+		InputStreamReader isr = null;
+		if (isCodechefModeOn) {
+			isr = new InputStreamReader(System.in);
+		} else {
+			String path = System.getProperty("user.dir");
+			String filePath = path + "/src/main/inputs/CHFTIRED.txt";
+			FileInputStream fis = new FileInputStream(filePath);
+			isr = new InputStreamReader(fis);
+		}
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+}
---
+++ src/test/java/org/muzir/codechef/practice/beginner/CHFTIREDTest.java
@@ -0,0 +1,1 @@
+package org.muzir.codechef.practice.beginner;import org.junit.Test;public class CHFTIREDTest {@Test public void test(){}}
